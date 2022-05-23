f3fb188a8b65aac7e41c6cbcd739e599a8e3c4f6
Mateusz Zebik <zebik.mateusz@gmail.com>
2022-05-23 19:54:41 +0200

    7.3 Stream of forum users list v.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,6 +7,8 @@ import com.kodilla.stream.forumUser.Forum;
 import com.kodilla.stream.forumUser.ForumUser;
 import com.kodilla.stream.person.People;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,9 +18,10 @@ public class StreamMain {
     public static void main(String[] args){
 
         Forum forum = new Forum();
+
         Map<Integer, ForumUser> theResultMapOfUsers = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'm')
-                .filter(forumUser -> forumUser.getBorn().getYear() < 2002)
+                .filter(forumUser -> Period.between(forumUser.getBorn(), LocalDate.now()).getYears() > 20)
                 .filter(forumUser -> forumUser.getPostsQuantity() > 1)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
