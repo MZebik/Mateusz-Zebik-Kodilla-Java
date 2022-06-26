a0d270da0efbce593292c73ec1e559677a16d377
Mateusz Zebik <zebik.mateusz@gmail.com>
2022-06-26 23:56:19 +0200

    12.3 Creat objects in SQL

--- kodilla-patterns/kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Board.java
+++ kodilla-patterns/kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Board.java
@@ -2,7 +2,7 @@ package com.kodilla.patterns.builder.checkers;
 
 public class Board {
     public final static int MIN_INDEX = 0;
-    public final static int MAX_INDEX = 9;
+    public final static int MAX_INDEX = 9 ;
     private final Figure[][] board = new Figure[10][];
 
     public Board() {
